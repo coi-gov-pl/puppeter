dfa74abd237eae891f3b8bc2e3aa9950be27a9eb
Suszyński Krzysztof <krzysztof.suszynski@coi.gov.pl>
2017-07-14 18:00:57 +0200

    Fixing test, that rely on Ubuntu

--- tests/persistence/test_os.py
+++ tests/persistence/test_os.py
@@ -1,7 +1,9 @@
 import re
 
+from puppeter import container
 from puppeter.domain.facter import Facter
-from puppeter.domain.model.osfacts import OsFamily, OperatingSystem, OperatingSystemRelease
+from puppeter.domain.model.osfacts import OsFamily, OperatingSystem, OperatingSystemRelease, OperatingSystemCodename
+from tests.domain.mock_facter import MockFacter
 
 
 def test_osfamily():
@@ -40,3 +42,15 @@ def test_osrelrelease_minor():
 
     assert minor is not None
     assert re.compile('^\d+$').match(minor) is not None
+
+
+def setup_function():
+    MockFacter.set_fact(OperatingSystemCodename, OperatingSystemCodename('trusty'))
+    MockFacter.set_fact(OperatingSystemRelease, OperatingSystemRelease('14.04'))
+    MockFacter.set_fact(OperatingSystem, OperatingSystem.Ubuntu)
+    MockFacter.set_fact(OsFamily, OsFamily.Debian)
+    container.initialize()
+
+
+def teardown_function():
+    MockFacter.reset_facts()
